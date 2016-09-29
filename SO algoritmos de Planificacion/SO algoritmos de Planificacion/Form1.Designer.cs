f70d89a6f566d01fc1535166ff6afc4f8f602c83
Mauricio360 <mayaur@miumg.edu.gt>
2016-09-28 19:09:05 -0600

    hola mundo

---
+++ SO algoritmos de Planificacion/SO algoritmos de Planificacion/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace SO_algoritmos_de_Planificacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listProcesos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtCpu = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbQuantum = new System.Windows.Forms.ComboBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.listEjecucion = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timerProcesos = new System.Windows.Forms.Timer(this.components);
+            this.trbVelocidad = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbVelocidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(115, 78);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(102, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listProcesos);
+            this.groupBox1.Controls.Add(this.txtCpu);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 374);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PROCESOS";
+            // 
+            // listProcesos
+            // 
+            this.listProcesos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listProcesos.Location = new System.Drawing.Point(8, 107);
+            this.listProcesos.Name = "listProcesos";
+            this.listProcesos.Size = new System.Drawing.Size(209, 261);
+            this.listProcesos.TabIndex = 7;
+            this.listProcesos.UseCompatibleStateImageBehavior = false;
+            this.listProcesos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "N";
+            this.columnHeader1.Width = 52;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Proceso";
+            this.columnHeader2.Width = 83;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "CPU";
+            this.columnHeader3.Width = 69;
+            // 
+            // txtCpu
+            // 
+            this.txtCpu.Location = new System.Drawing.Point(79, 43);
+            this.txtCpu.Name = "txtCpu";
+            this.txtCpu.Size = new System.Drawing.Size(99, 20);
+            this.txtCpu.TabIndex = 6;
+            this.txtCpu.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCpu_KeyPress);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(79, 19);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(99, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CPU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLimpiar);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.cbQuantum);
+            this.groupBox2.Controls.Add(this.btnIniciar);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 97);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PROCESADOR";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quantum";
+            // 
+            // cbQuantum
+            // 
+            this.cbQuantum.FormattingEnabled = true;
+            this.cbQuantum.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cbQuantum.Location = new System.Drawing.Point(6, 37);
+            this.cbQuantum.Name = "cbQuantum";
+            this.cbQuantum.Size = new System.Drawing.Size(172, 21);
+            this.cbQuantum.TabIndex = 1;
+            this.cbQuantum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cbQuantum_KeyPress);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(115, 68);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(102, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // listEjecucion
+            // 
+            this.listEjecucion.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4});
+            this.listEjecucion.Location = new System.Drawing.Point(262, 63);
+            this.listEjecucion.Name = "listEjecucion";
+            this.listEjecucion.Size = new System.Drawing.Size(651, 426);
+            this.listEjecucion.TabIndex = 8;
+            this.listEjecucion.UseCompatibleStateImageBehavior = false;
+            this.listEjecucion.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "PROCESO";
+            this.columnHeader4.Width = 645;
+            // 
+            // timerProcesos
+            // 
+            this.timerProcesos.Tick += new System.EventHandler(this.timerProcesos_Tick);
+            // 
+            // trbVelocidad
+            // 
+            this.trbVelocidad.Location = new System.Drawing.Point(262, 12);
+            this.trbVelocidad.Maximum = 2000;
+            this.trbVelocidad.Minimum = 200;
+            this.trbVelocidad.Name = "trbVelocidad";
+            this.trbVelocidad.Size = new System.Drawing.Size(651, 45);
+            this.trbVelocidad.SmallChange = 200;
+            this.trbVelocidad.TabIndex = 9;
+            this.trbVelocidad.TickFrequency = 200;
+            this.trbVelocidad.Value = 200;
+            this.trbVelocidad.Scroll += new System.EventHandler(this.trbVelocidad_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(859, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Lento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(277, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Rapido";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(9, 68);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 12;
+            this.btnLimpiar.Text = "REINICIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Visible = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(925, 500);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trbVelocidad);
+            this.Controls.Add(this.listEjecucion);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ROUND ROBIN GRUPO #5 SO2016B";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbVelocidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listProcesos;
+        private System.Windows.Forms.TextBox txtCpu;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbQuantum;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ListView listEjecucion;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Timer timerProcesos;
+        private System.Windows.Forms.TrackBar trbVelocidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ SO algoritmos de Planificacion/SO algoritmos de Planificacion/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SO_algoritmos_de_Planificacion
+{
+    public partial class Form1 : Form
+    {
+        int nProceso = 0,quantum=0;
+
+        string[] proces = new string[200];
+        int[] cpu = new int[200];
+        Color[] colorProceso = new Color[200];        
+
+   
+
+        public Form1()
+        {
+            InitializeComponent();
+            colorProceso[0] = Color.AliceBlue;
+            colorProceso[1] = Color.AntiqueWhite;
+            colorProceso[2] = Color.DarkBlue;
+            colorProceso[3] = Color.SaddleBrown;
+            colorProceso[4] = Color.DimGray;
+            colorProceso[5] = Color.PapayaWhip;
+            colorProceso[6] = Color.Green;
+            colorProceso[7] = Color.Honeydew;
+            colorProceso[8] = Color.Aquamarine;
+            colorProceso[9] = Color.PaleGreen;
+            colorProceso[10] = Color.Navy;
+            colorProceso[11] = Color.Tan;
+            colorProceso[12] = Color.Red;
+            colorProceso[13] = Color.Pink;
+            colorProceso[14] = Color.Violet;
+            colorProceso[15] = Color.Snow;
+            colorProceso[16] = Color.CadetBlue;
+            colorProceso[17] = Color.Khaki;
+            colorProceso[18] = Color.CadetBlue;
+            colorProceso[19] = Color.DeepSkyBlue;
+            colorProceso[20] = Color.MediumPurple;
+            colorProceso[21] = Color.MistyRose;
+            colorProceso[22] = Color.Azure;
+            colorProceso[23] = Color.Silver;
+            colorProceso[24] = Color.SeaShell;
+            colorProceso[25] = Color.Wheat;          
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "" && txtCpu.Text != "")
+            {
+                ListViewItem proc = new ListViewItem();
+                proc.Text = nProceso.ToString();
+                proc.SubItems.Add(txtNombre.Text);
+                proc.SubItems.Add(txtCpu.Text);
+
+                if (nProceso <= 25)
+                {
+                    proc.BackColor = colorProceso[nProceso];
+                }
+                else
+                {
+                    Random rd = new Random();
+                    int valor = rd.Next(0, 25);
+                    proc.BackColor = colorProceso[valor];                       
+                }
+
+                listProcesos.Items.Add(proc);
+
+                proces[nProceso] = txtNombre.Text;
+                cpu[nProceso] = Convert.ToInt32( txtCpu.Text);
+                
+                nProceso++;                
+
+                txtCpu.Clear();
+                txtNombre.Clear();       
+            }
+            else
+            {
+                MessageBox.Show("Debe llenar todos los campos requeridos");
+            }
+ 
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (btnIniciar.Text == "INICIAR")
+            {
+                if (cbQuantum.Text != "")
+                {
+                    if (proces[0] != null)
+                    {
+                        timerProcesos.Enabled = true;
+                        quantum = Convert.ToInt32(cbQuantum.Text);
+                        btnIniciar.Text = "PAUSA";
+                        cbQuantum.Enabled = false;
+                        btnLimpiar.Visible = true;
+                    }
+                }
+                else { MessageBox.Show("Indique el Quantum"); }
+
+            }
+            else
+            {
+                timerProcesos.Enabled = false;
+                btnIniciar.Text = "INICIAR";
+                cbQuantum.Enabled = true;
+                btnLimpiar.Visible = false;          
+            }
+           
+        }
+
+        private void txtCpu_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar))
+            {
+            }else if (char.IsControl(e.KeyChar)|| char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void cbQuantum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar))
+            {
+            }
+            else if (char.IsControl(e.KeyChar) || char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void trbVelocidad_Scroll(object sender, EventArgs e)
+        {
+            timerProcesos.Interval = trbVelocidad.Value;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            listEjecucion.Items.Clear();
+            listProcesos.Items.Clear();
+            nProceso = 0;
+            quantum = 0;
+            cbQuantum.Text = "";
+            btnLimpiar.Visible = false;
+        }
+
+        private void timerProcesos_Tick(object sender, EventArgs e)
+        {
+            for(int j = 0; j < nProceso; j++)
+            {
+                Thread.Sleep(timerProcesos.Interval/10);
+                if (cpu[j] > 0)
+                {
+                    if(cpu[j]<quantum)
+                    {
+                        for(int s = 1; s <= cpu[j]; s++)
+                        {
+                            ListViewItem proc = new ListViewItem();
+                            proc.Text = proces[j];
+                            proc.BackColor = colorProceso[j];
+                            listEjecucion.Items.Add(proc);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 1; i <= quantum; i++)
+                        {
+                            ListViewItem proc = new ListViewItem();
+                            proc.Text = proces[j];
+                            proc.BackColor = colorProceso[j];
+                            listEjecucion.Items.Add(proc);
+                        }
+                    }
+                   
+                    cpu[j] = cpu[j] - quantum;
+                }
+
+
+            }
+
+            
+        }
+
+
+
+        //---------------------------------
+    }//----------------------------------
+}
